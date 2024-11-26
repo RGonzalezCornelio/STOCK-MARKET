d2f80fce29befe7fe3e428f7e7890ebeb543cabf
Rodrigo Gonzalez <rodrigorojo@hotmail.es>
2024-11-26 16:31:57 +0100

    Prueba 2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ y = int(open('./counter.txt').readline())
 linkstarter = "https://finance.yahoo.com/quote/"
 headers = {"User-Agent":"Mozilla/5.0"}
 
-for x in range(y, 5):
+for x in range(y, 10):
 
     print(Fore.RESET + "processing "+ str(x) + "... ", end= '')
     jsonf = {} #Guardaremos la informacion en un json
@@ -67,13 +67,35 @@ for x in range(y, 5):
     trailing_PE = str(data.find_all('td', class_="yf-kbx2lo")[15]).split('>')[1].split('<')[0]
     forward_PE = str(data.find_all('td', class_="yf-kbx2lo")[22]).split('>')[1].split('<')[0]
     enterprise_value_EBITDA = str(data.find_all('td', class_="yf-kbx2lo")[57]).split('>')[1].split('<')[0]
-    print("market_cap: " + market_cap + "Enterprise value: " + enterprise_value + "Trailing P/E: " + trailing_PE + "forward_PE: " + forward_PE + "enterprise_value_EBITDA: " + enterprise_value_EBITDA)
+    #print("market_cap: " + market_cap + "Enterprise value: " + enterprise_value + "Trailing P/E: " + trailing_PE + "forward_PE: " + forward_PE + "enterprise_value_EBITDA: " + enterprise_value_EBITDA)
 
     jsonf["Market Cap"] = market_cap
     jsonf["Enterprise Value"] = enterprise_value
     jsonf["Trailing P/E"] = trailing_PE
     jsonf["Forward P/E"] = forward_PE
     jsonf["Enterprise Value/EBITDA"] = enterprise_value_EBITDA
+
+
+    profit_margin = str(data.find_all('td', class_="value yf-vaowmx")[2]).split('>')[1].split('<')[0]
+    operating_margin = str(data.find_all('td', class_="value yf-vaowmx")[3]).split('>')[1].split('<')[0]
+    quarterly_revenue_growth = str(data.find_all('td', class_="value yf-vaowmx")[8]).split('>')[1].split('<')[0]
+    quarterly_earnings_growth = str(data.find_all('td', class_="value yf-vaowmx")[13]).split('>')[1].split('<')[0]
+    beta = str(data.find_all('td', class_="value yf-vaowmx")[22]).split('>')[1].split('<')[0]
+    percentage_by_insiders = str(data.find_all('td', class_="value yf-vaowmx")[34]).split('>')[1].split('<')[0]
+    forward_annual_divident_rate = str(data.find_all('td', class_="value yf-vaowmx")[41]).split('>')[1].split('<')[0]
+    five_year_divident_rate = str(data.find_all('td', class_="value yf-vaowmx")[45]).split('>')[1].split('<')[0]
+    #print("profit_margin: " + profit_margin + " operating_margin: " + operating_margin + " quarterly_revenue_growth: " + quarterly_revenue_growth + " quarterly_earnings_growth: " + quarterly_earnings_growth\
+    #       + " beta: " + beta + " percentage_by_insiders: " + percentage_by_insiders + " forward_annual_divident_rate" + forward_annual_divident_rate + " five_year_divident_rate: " + five_year_divident_rate)
+
+    jsonf["Profit Margin"] = profit_margin
+    jsonf["Operating Margin"] = operating_margin
+    jsonf["Quarterly Revenue Growth"] = quarterly_revenue_growth
+    jsonf["Quarterly Earnings Growth"] = quarterly_earnings_growth
+    jsonf["Beta (5Y Monthly)"] = beta
+    jsonf["Percentage by insiders"] = percentage_by_insiders
+    jsonf["Forward Annual Dividend Rate"] = forward_annual_divident_rate
+    jsonf["5 Year Average Dividend Yield"] = five_year_divident_rate
+    
 
     jsonfile = open('./DataJSON/' + str(x) + '.json', 'w')
     json.dump(jsonf, jsonfile)
